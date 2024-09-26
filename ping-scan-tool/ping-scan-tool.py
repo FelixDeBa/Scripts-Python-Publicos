58e7387c1dfad8df06e45f9a6c67be20f8a63968
Félix Delgadillo Barrios <38565840+FelixDeBa@users.noreply.github.com>
2024-09-26 11:40:30 -0600

    Update ping-scan-tool.py

--- ping-scan-tool/ping-scan-tool.py
+++ ping-scan-tool/ping-scan-tool.py
@@ -1,8 +1,10 @@
 import openpyxl, re, subprocess
-from datetime import date
+from datetime import date, timedelta
 from openpyxl.styles import PatternFill
 import os
 from time import sleep
+from tkinter import filedialog
+
 os.system("")
 
 class bcolors:
@@ -16,19 +18,24 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-hoy = date.today()
 
-doc = openpyxl.Workbook()
-hoja = doc['Sheet']
-hoja.title = 'ips'
+def get_last_fila(hoja, col):
+    last_fila = 1
+    while not hoja[f'{col}{last_fila}'].value is None:
+        last_fila+=1
+    return last_fila
+
+def str_rango_semana(fecha):
+    meses=('enero', 'febrero', 'marzo', 'abril', 'mayo','junio','julio','agosto','septiembre','agosto','octubre','noviembre','diciembre')
+    lunes = fecha - timedelta(fecha.weekday())
+    domingo = fecha + timedelta(6 - fecha.weekday()) 
+    return f'{lunes.day} de {meses[lunes.month-1][:3]}-{domingo.day} de {meses[domingo.month-1][:3]}'
+
+
 LINE_UP = '\033[1A'
 LINE_CLEAR = '\x1b[2K'
-
-hoja['a1'].value ='Fecha Escaneo'
-hoja['b1'].value ='IP'
-hoja['c1'].value ='Nombre dominio'
-hoja['d1'].value ='Información Adicional'
-
+hoy = date.today()
+str_semana = str_rango_semana(hoy)
 
 regexUsr = r'(?:Haciendo ping a |Pinging )(?P<dns>(?:.+)(?!\d{1,3}(?:\.\d{1,3}){3}))?(?: \[)?(?P<ip>\d{1,3}(?:\.\d{1,3}){3})(?:\])?'
 regexIP = r'^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
@@ -52,9 +59,55 @@ else:
 
 octetos_fin = [int(dato) for dato in ip_fin.split('.')]
 
-doclog=f'LogEscaneo {ip_inicio}_{ip_fin}_{str(hoy)}.log'
+pregunta = input("Desea crear un nuevo documento (Por defecto) o usar uno existente (E)? ")
 
-fila = 2
+if pregunta.lower() == "e":
+    arch_ok=False
+    while not arch_ok:
+        nombredoc = filedialog.askopenfilename(title="Selecciona el archivo base")
+        if nombredoc == "":
+            print("No se seleccionó un archivo: ")
+            arch_ok=False
+        else:
+            if (nombredoc.split('/')[-1]).split('.')[-1] in ['xlsx','xlsm','xls']:
+                arch_ok=True
+            else:
+                arch_ok=False
+                print('No seleccionaste un archivo compatible con excel')
+    
+    doc = openpyxl.load_workbook(nombredoc)
+    hoja_enc = False
+    for hoja_temp in doc.sheetnames:
+        if [doc[hoja_temp][f'{chr(col+97)}1'].value for col in range(0,4)] == ['Fecha Escaneo', 'IP', 'Nombre Dominio', 'Informacion Adicional']:
+            print(f'Trabajando sobre el archivo {nombredoc.split("/")[-1]} en la hoja {hoja_temp.title()}')
+            hoja_enc = True
+            hoja = doc[hoja_temp]
+            fila = get_last_fila(doc[hoja_temp], 'a')
+            break
+    if not hoja_enc:
+        hoja = doc.create_sheet('IPs' if not 'IPs' in doc.sheetnames else 'IPs_copia_')
+        print(f'Trabajando sobre el archivo {nombredoc.split("/")[-1]} en la nueva hoja {hoja.title()}')
+        hoja['a1'].value ='Fecha Escaneo'
+        hoja['b1'].value ='IP'
+        hoja['FW-MTYc1'].value ='Nombre Dominio'
+        hoja['d1'].value ='Informacion Adicional'
+        fila = 2
+else:
+    doc = openpyxl.Workbook()
+    hoja = doc['Sheet']
+    hoja.title = 'IPs'
+
+    hoja['a1'].value ='Fecha Escaneo'
+    hoja['b1'].value ='IP'
+    hoja['c1'].value ='Nombre dominio'
+    hoja['d1'].value ='Informacion Adicional'
+    fila = 2
+    print(f'Trabajando sobre nuevo archivo Escaneo_{str_semana}.xlsx en la hoja {hoja.title()}')
+    nombredoc = f'Escaneo_{str_semana}.xlsx'
+
+
+doclog=f'LogEscaneo {str(hoy)}.log'
+
 for oct1 in range(octetos_inicio[0], octetos_fin[0]+1):
     for oct2 in range(octetos_inicio[1], octetos_fin[1]+1):
         for oct3 in range(octetos_inicio[2], octetos_fin[2]+1):
@@ -112,7 +165,6 @@ for oct1 in range(octetos_inicio[0], octetos_fin[0]+1):
                 log.close()
 
 sleep(2)
-nombredoc=f"Escaneo {ip_inicio}_{ip_fin}_{str(hoy)}.xlsx"
 log = open(doclog,'a')
 try:
     doc.save(nombredoc)
